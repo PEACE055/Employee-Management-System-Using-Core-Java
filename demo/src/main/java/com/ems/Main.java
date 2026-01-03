94a87e29df712824f9ab1cfe471aa795e62b18c7
Sujal Patil <sujalpatil5478@gmail.com>
2026-01-04 00:37:25 +0530

    second commit

--- demo/src/main/java/com/ems/Main.java
+++ demo/src/main/java/com/ems/Main.java
@@ -7,5 +7,6 @@ public class Main {
     public static void main(String[] args) {
 
         System.out.println("Be Consistent");
+        System.out.println("Be Desciplined Person in this world their is the only one way to win this fucking simple gme ");
     }
 }
